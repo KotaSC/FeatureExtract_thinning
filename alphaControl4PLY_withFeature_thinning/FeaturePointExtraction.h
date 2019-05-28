53529388b2b78d4b779b7d9a3255e4fa71fcc5f3
Kota Kawakami <is0302ke@gmail.com>
2019-05-28 22:28:02 +0900

    Add files via upload

---
+++ alphaControl4PLY_withFeature_thinning/FeaturePointExtraction.h
@@ -0,0 +1,28 @@
+#ifndef _FeaturePointExtraction_H__
+#define _FeaturePointExtraction_H__
+
+#include <kvs/PolygonObject>
+#include <kvs/PointObject>
+#include <kvs/Module>
+#include <vector>
+
+class FeaturePointExtraction: public kvs::PointObject {
+  kvsModuleSuperClass( kvs::PointObject );
+
+ public:
+  FeaturePointExtraction( void );
+  FeaturePointExtraction( kvs::PolygonObject* ply,
+						  std::vector<float> &ft,
+						  double ft_ratio,
+						  double threshold );
+ private:
+  void alpbaControl4Feature( kvs::PolygonObject* ply,
+						     std::vector<float> &ft,
+						     double ft_ratio,
+						     double threshold );
+};
+
+
+
+
+#endif
