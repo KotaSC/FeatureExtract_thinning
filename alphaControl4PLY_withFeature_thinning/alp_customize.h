16b42c697435d9b53e5575cd9ec355a0c5541a39
Kota Kawakami <is0302ke@gmail.com>
2019-06-27 17:11:00 +0900

    Add files via upload

--- alphaControl4PLY_withFeature_thinning/alp_customize.h
+++ alphaControl4PLY_withFeature_thinning/alp_customize.h
@@ -6,9 +6,10 @@ const std::string allowedFileType[] = { "ply", "spbr", "xyz", "pbr" };
 //------- Default Setting --------------------------
 const int REPEAT_LEVEL       = 100;
 const int IMAGE_RESOLUTION   = 2000;
-const double OPACITY         = 0.4;
+const double OPACITY         = 0.2;
 const double FEATURE_OPACITY = 0.9;
-const double THRESHOLD       = 0.02;
+const double THRESHOLD       = 0.00;
+const bool THINNING          = false;
 const char OUT_PBR_FILE[]    = "../SPBR_DATA/out-";
 //---------------------------------------------------
 
