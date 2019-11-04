7ab55c29563f438c4381e83f8223f0c5b06b9229
Kota Kawakami <is0302ke@gmail.com>
2019-11-05 00:51:38 +0900

    .gitignore is now working

--- alphaControl4PLY_withFeature_v005/alp_customize.h
+++ alphaControl4PLY_withFeature_v005/alp_customize.h
@@ -9,7 +9,7 @@ const int IMAGE_RESOLUTION   = 1000;
 const double OPACITY         = 0.2;
 const double FEATURE_OPACITY = 0.9;
 const double THRESHOLD       = 0.05;
-const char OUT_PBR_FILE[]    = "../SPBR_DATA/out-"; 
+const char OUT_PBR_FILE[]    = ""../SPBR_DATA/out-";
 //---------------------------------------------------
 
 #endif
