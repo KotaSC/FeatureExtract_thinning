4f0a024dbacee649ce4285297a0fe345eef7fb95
Kota Kawakami <is0302ke@gmail.com>
2019-12-06 16:49:57 +0900

    update

--- alphaControl4PLY_withFeature_v005/alp_customize.h
+++ alphaControl4PLY_withFeature_v005/alp_customize.h
@@ -5,10 +5,10 @@ const std::string allowedFileType[] = { "ply", "spbr", "xyz", "pbr" };
 
 //------- Default Setting --------------------------
 const int REPEAT_LEVEL       = 100;
-const int IMAGE_RESOLUTION   = 1000;
+const int IMAGE_RESOLUTION   = 1024;
 const double OPACITY         = 0.2;
 const double FEATURE_OPACITY = 0.9;
-const double THRESHOLD       = 0.35;
+const double THRESHOLD       = 0.3;
 const char OUT_PBR_FILE[]    = "";
 const char DIR_NAME[]        = "";
 //---------------------------------------------------
