0b39d3414f0a80a00135f7c54521dffc61fa27c7
Kota Kawakami <is0302ke@gmail.com>
2020-09-28 00:41:09 +0900

    update

--- alphaControl4PLY_withFeature_thinning/FeaturePointExtraction.cpp
+++ alphaControl4PLY_withFeature_thinning/FeaturePointExtraction.cpp
@@ -139,8 +139,32 @@ void FeaturePointExtraction::adaptiveAlphaControl4Feature( kvs::PolygonObject *p
     }
   }
 
+  double alphaMin = ALPHA_MIN;
+  double alphaMax = ALPHA_MAX;
+  double largeFth = LARGE_F_TH;
+  double dim = DIMENSION;
+  double initialPoint = largeFth - smallFth;
+  double grad = (alphaMax - alphaMin) / std::pow(initialPoint, dim);
+
+  std::cout << "===================================" << std::endl;
+  std::cout << "Minimum opacity          : " << alphaMin << std::endl;
+  std::cout << "Maximum opacity          : " << alphaMax << std::endl;
+  std::cout << "Small threshold          : " << smallFth << std::endl;
+  std::cout << "Large threshold          : " << largeFth << std::endl;
+  std::cout << "Dimension                : " << dim << std::endl;
+  std::cout << "Number of feature points : " << num << std::endl;
+  std::cout << "===================================" << std::endl;
+
   ply->updateMinMaxCoords();
-  kvs::ValueArray<kvs::Real32> coords = ply->coords();
+
+  kvs::ValueArray<kvs::Real32> coords  = ply->coords();
+  kvs::ValueArray<kvs::Real32> normals = ply->normals();
+  kvs::ValueArray<kvs::UInt8> colors   = ply->colors();
+
+  std::vector<kvs::Real32> SetCoords;
+  std::vector<kvs::Real32> SetNormals;
+  std::vector<kvs::UInt8>  SetColors;
+
   float *pdata = coords.data();
   kvs::Vector3f minBB = ply->minObjectCoord();
   kvs::Vector3f maxBB = ply->maxObjectCoord();
@@ -171,17 +195,73 @@ void FeaturePointExtraction::adaptiveAlphaControl4Feature( kvs::PolygonObject *p
   double radius    = b_leng / div;
 
   std::cout << "Start OCtree Search..... " << std::endl;
-  for ( size_t i = 0; i < numVert; i++ )
+  for ( size_t i = 0; i < num; i++ )
   {
-    if (i == numVert)
+    if ( i == num )
       --i;
-    double point[3] = { coords[3 * i],
-                        coords[3 * i + 1],
-                        coords[3 * i + 2] };
+
+    size_t index = ind[i];
+
+    double point[3] = { coords[3 * index],
+                        coords[3 * index + 1],
+                        coords[3 * index + 2] };
 
     vector<size_t> nearInd;
     vector<double> dist;
     search_points( point, radius, pdata, myTree->octreeRoot, &nearInd, &dist );
     int n0 = (int)nearInd.size();
+
+    double sumNearestFt = 0.0;
+    double aveNearestFt = 0.0;
+
+    for ( size_t j = 0; j < n0; j++ ) {
+      sumNearestFt += ft[ nearInd[j] ];
+    }
+
+    aveNearestFt = sumNearestFt / n0;
+
+    double x = ft[index] - smallFth;
+    double alpha = grad * std::pow(x, dim) + alphaMin;
+
+    if (ft[index] >= LARGE_F_TH)
+      alpha = alphaMax;
+
+    // Caluculate Point Number and Increase Ratio according to Feature Value
+    double a_num = fpoint->calculateRequiredPartcleNumber(alpha, repeatLevel, BBMin, BBMax);
+    double ratio = fpoint->pointRatio(a_num);
+    double createNum = 1.0 * ratio;
+
+    if (!((i + 1) % INTERVAL))
+    {
+      std::cout << i + 1 << std::endl;
+      std::cout << "Feature Value:        " << ft[index] << std::endl;
+      std::cout << "Alpha:                " << alpha << std::endl;
+      std::cout << "Analytical Point Num: " << a_num << std::endl;
+      std::cout << "Point Ratio:          " << ratio << std::endl;
+      std::cout << "Create Point Num:     " << createNum << std::endl;
+    }
+
+    for (int j = 0; j < createNum; j++)
+    {
+
+      SetCoords.push_back(coords[3 * index]);
+      SetCoords.push_back(coords[3 * index + 1]);
+      SetCoords.push_back(coords[3 * index + 2]);
+
+      SetNormals.push_back(normals[3 * index]);
+      SetNormals.push_back(normals[3 * index + 1]);
+      SetNormals.push_back(normals[3 * index + 2]);
+
+      SetColors.push_back(colors[3 * index]);
+      SetColors.push_back(colors[3 * index + 1]);
+      SetColors.push_back(colors[3 * index + 2]);
+
+      // SetColors.push_back( 255 );
+      // SetColors.push_back( 0 );
+      // SetColors.push_back( 0 );
+    }
   }
+  SuperClass::setCoords(kvs::ValueArray<kvs::Real32>(SetCoords));
+  SuperClass::setNormals(kvs::ValueArray<kvs::Real32>(SetNormals));
+  SuperClass::setColors(kvs::ValueArray<kvs::UInt8>(SetColors));
 }
